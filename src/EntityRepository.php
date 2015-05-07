73da52fcc65dcfc05ce1a453ceb2424b8081c2c3
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-05-07 22:26:33 +0200

    Repository persistence has been decoupled

--- src/EntityRepository.php
+++ src/EntityRepository.php
@@ -3,10 +3,9 @@
 namespace GibbonCms\Gibbon;
 
 use GibbonCms\Gibbon\Interfaces\Factory as FactoryInterface;
-use GibbonCms\Gibbon\Interfaces\PersistableRepository;
-use ReflectionObject;
+use GibbonCms\Gibbon\Interfaces\Repository;
 
-class EntityRepository implements PersistableRepository
+class EntityRepository implements Repository
 {
     /**
      * The filesystem is used to read and write files
@@ -67,74 +66,6 @@ class EntityRepository implements PersistableRepository
     }
 
     /**
-     * Return a list of all ids
-     * 
-     * @return array
-     */
-    public function getList()
-    {
-        return array_keys($this->cache->all());
-    }
-
-    /**
-     * Save an entity
-     * 
-     * @param mixed
-     * @return bool
-     */
-    public function save($entity)
-    {
-        $fresh = is_null($entity->getId());
-
-        if ($fresh) {
-            $this->setNewId($entity, $this->generateId());
-        }
-
-        $success = $this->filesystem->put(
-            "{$entity->getId()}-{$entity->getSlug()}.md", 
-            $this->factory->encode($entity)
-        );
-
-        if ($success) {
-            $this->cache->put($entity->getId(), $entity);
-            $this->cache->persist();
-        }
-
-        return $success;
-    }
-
-    /**
-     * Delete an entity
-     * 
-     * @return bool
-     */
-    public function delete($entity)
-    {
-        $success = $this->filesystem->delete("{$entity->getId()}-{$entity->getSlug()}.md");
-
-        if ($success) {
-            $this->cache->forget($entity->getId());
-            $this->cache->persist();
-        }
-
-        return $success;
-    }
-
-    /**
-     * Copy an entity
-     * 
-     * @param mixed $entity
-     * @return mixed
-     */
-    public function copy($entity)
-    {
-        $copy = clone $entity;
-        $this->setNewId($copy);
-
-        return $copy;
-    }
-
-    /**
      * Build the cache
      * 
      * @return void
@@ -147,6 +78,7 @@ class EntityRepository implements PersistableRepository
 
         foreach ($files as $file) {
             $entity = $this->parseFile($file);
+            if ($entity != null) $this->cache->put($entity->getId(), $entity);
         }
         
         $this->cache->persist();
@@ -159,7 +91,12 @@ class EntityRepository implements PersistableRepository
     protected function parseFile($file)
     {
         if ($file['extension'] == 'md') {
-            $parsedFilename = $this->parseFilename($file['filename']);
+            $parts = explode('-', $file['filename'], 2);
+
+            $parsedFilename = [
+                'id'   => $parts[0],
+                'slug' => $parts[1],
+            ];
 
             $entity = $this->factory->make([
                 'id'   => $parsedFilename['id'],
@@ -167,56 +104,7 @@ class EntityRepository implements PersistableRepository
                 'data' => $this->filesystem->read($file['path']),
             ]);
             
-            $this->cache->put($entity->getId(), $entity);
-
             return $entity;
         }
-    }
-
-    /**
-     * Extract the id and slug from a filename
-     * 
-     * @param string $filename
-     * @return array
-     */
-    protected function parseFilename($filename)
-    {
-        $parts = explode('-', $filename, 2);
-
-        return [
-            'id'   => $parts[0],
-            'slug' => $parts[1],
-        ];
-    }
-
-    /**
-     * Generate an id for a new entity
-     * 
-     * @return int
-     */
-    protected function generateId()
-    {
-        $list = $this->getList();
-
-        $last = array_pop($list);
-
-        return intval($last) + 1;
-    }
-
-    /**
-     * Set a new, valid id on an entity
-     * 
-     * @param mixed $entity
-     * @param mixed $id
-     * @return mixed
-     */
-    protected function setNewId($entity, $id = null)
-    {
-        $reflection = new ReflectionObject($entity);
-        $property = $reflection->getProperty('id');
-        $property->setAccessible(true);
-        $property->setValue($entity, $id);
-
-        return $entity;
     }
 }
---
+++ tests/PersistableEntityRepositoryTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace GibbonCms\Gibbon\Tests;
+
+use GibbonCms\Gibbon\Cache;
+use GibbonCms\Gibbon\Filesystem;
+use GibbonCms\Gibbon\PersistableEntityRepository;
+use GibbonCms\Gibbon\Tests\Stubs\Entity;
+use GibbonCms\Gibbon\Tests\Stubs\EntityFactory;
+
+class PersistableEntityRepositoryTest extends TestCase
+{
+    function setUp()
+    {
+        $this->repository = new PersistableEntityRepository(
+            new Filesystem($this->fixtures . '/entities/'),
+            new Cache($this->fixtures . '/entities/.cache'),
+            new EntityFactory
+        );
+
+        $this->repository->buildCache();
+    }
+
+    /** @test */
+    function it_is_initializable()
+    {
+        $this->assertInstanceOf(PersistableEntityRepository::class, $this->repository);
+    }
+
+    /** @test */
+    function it_saves_an_entity()
+    {
+        $entity = new Entity('stub', 'lorem ipsum dolor sit');
+        $this->assertTrue($this->repository->save($entity));
+
+        $this->assertEquals($entity->getId(), $this->repository->find($entity->getId())->getId());
+     
+        @unlink($this->fixtures . '/entities/' . $entity->getId() . '-stub.md');
+    }
+
+    /** @test */
+    function it_updates_an_existing_entity()
+    {
+        $entity = new Entity('stub', 'lorem ipsum dolor sit');
+        $this->repository->save($entity);
+
+        $entity->setData('foo bar baz');
+        $this->assertTrue($this->repository->save($entity));
+
+        $this->assertEquals('foo bar baz', $this->repository->find($entity->getId())->getData());
+
+        @unlink($this->fixtures . '/entities/' . $entity->getId() . '-stub.md');
+    }
+
+    /** @test */
+    function it_deletes_an_entity()
+    {
+        $entity = new Entity('stub', 'lorem ipsum dolor sit');
+        $this->repository->save($entity);
+        $this->assertTrue($this->repository->delete($entity));
+    }
+
+    /** @test */
+    function it_can_copy_an_entity()
+    {
+        $entity = $this->repository->find(1);
+        $copy = $this->repository->copy($entity);
+        $this->assertEquals(null, $copy->getId());
+
+        $this->repository->save($copy);
+        $this->assertEquals(3, $copy->getId());
+        
+        @unlink($this->fixtures . '/entities/' . $copy->getId() . '-lorem-ipsum.md');
+    }
+}
